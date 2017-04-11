f320cedf5552001a4e6e24ad653afbf52f31cf5c
criptis <criptis@users.noreply.github.com>
2017-04-11 08:33:39 +0300

    FIX - Dont allow to hace duplicate stocks

--- app/src/main/java/com/udacity/stockhawk/ui/MainActivity.java
+++ app/src/main/java/com/udacity/stockhawk/ui/MainActivity.java
@@ -124,6 +124,8 @@ public class MainActivity extends AppCompatActivity implements LoaderManager.Loa
     void addStock(String symbol) {
         if (symbol != null && !symbol.isEmpty()) {
 
+            symbol = symbol.toUpperCase();
+
             if (networkUp()) {
                 swipeRefreshLayout.setRefreshing(true);
             } else {
